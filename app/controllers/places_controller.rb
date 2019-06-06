bf605609cf2f1bec80f33755a939ac37ed0af70c
alexandra.gergar@gmail.com <alexandra.gergar@gmail.com>
2019-06-06 23:38:50 +0000

    simple form

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,4 +3,22 @@ class PlacesController < ApplicationController
   def index
     @pagy, @places = pagy(Place.all, items: 5)
   end
+
+  def new
+    @place = Place.new
+  end
+
+  def create
+    Place.create(place_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
+  end
+
+
+
 end

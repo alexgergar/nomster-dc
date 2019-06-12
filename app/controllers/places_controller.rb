c63063465a660112a6114ea3ced4ee2e9a6ef341
alexandra.gergar@gmail.com <alexandra.gergar@gmail.com>
2019-06-12 20:12:08 +0000

    l26 Destroy premissions

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   
   def index
     @pagy, @places = pagy(Place.all, items: 5)
@@ -30,6 +30,7 @@ class PlacesController < ApplicationController
     @place = Place.find(params[:id])
     if @place.user != current_user
       return render plain: 'Not Allowed', status: :forbidden
+    end
 
     @place.update_attributes(place_params)
     redirect_to root_path
@@ -37,6 +38,10 @@ class PlacesController < ApplicationController
 
   def destroy
     @place = Place.find(params[:id])
+    if @place.user != current_user
+      return render plain: 'Not Allowed', status: :forbidden
+    end
+    
     @place.destroy
     redirect_to root_path
   end

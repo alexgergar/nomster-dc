bf18f956d08dfbe76a88115db7750239d0930f08
alexandra.gergar@gmail.com <alexandra.gergar@gmail.com>
2019-06-25 19:46:47 +0000

    L43

--- test/mailers/previews/notification_mailer_preview.rb
+++ test/mailers/previews/notification_mailer_preview.rb
@@ -1,4 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/notification_mailer
 class NotificationMailerPreview < ActionMailer::Preview
-
+  def comment_added
+    NotificationMailer.comment_added
+  end
 end
